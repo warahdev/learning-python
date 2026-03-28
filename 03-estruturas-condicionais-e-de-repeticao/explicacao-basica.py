4d8f18148ad2cd72d5866fa138be008b9695a90a
stl-maria <emfj.estela@gmail.com>
2026-03-27 23:56:03 -0300

    feat: add new file

---
+++ 03-estruturas-condicionais-e-de-repeticao/explicacao-basica.py
@@ -0,0 +1,28 @@
+# Estrutura condicional
+
+    # Decisões
+opcao_escolhida = input(f'Escolha uma opção: \n1 - Cadastrar restaurante \n2 - Listar restaurante \nAtivar restaurante \n')  # armazenando a entrada de dado dentro de uma variavel
+
+if opcao_escolhida == 1:
+    print("Cadastrar restaurante")
+elif opcao_escolhida == 2:
+    print("Listar restaurante")
+elif opcao_escolhida == 3:
+    print("Ativar restaurante")
+else:
+    print("Encerrando o programa.")
+
+    # Para casos multiplos
+
+
+
+# Estrutura de repetição
+
+
+# Função range()
+
+
+# Operadpr tenário
+
+
+# Break e continue
